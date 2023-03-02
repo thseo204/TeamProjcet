33427091b3481f125f40e57df25a03c9d46a3bc4
USER <nanovia3@naver.com>
2023-03-02 18:09:14 +0900

    알라딘 베스트셀러 API 적용

---
+++ src/main/java/project/bookservice/openapi/GetAPIData.java
@@ -0,0 +1,67 @@
+package project.bookservice.openapi;
+
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class GetAPIData {
+    public String getTagvalue(String tag, Element eElement) {
+        NodeList nlList = eElement.getElementsByTagName(tag).item(0).getChildNodes();
+        Node nValue = (Node) nlList.item(0);
+        if (nValue == null) {
+            return null;
+        }
+        return nValue.getNodeValue();
+    }
+
+    public void getApiData() {
+        try {
+            String url = "http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbnanovia31701002" +
+                    "&QueryType=Bestseller" +
+                    "&MaxResults=10" +
+                    "&start=1" +
+                    "&SearchTarget=Book" +
+                    "&output=xml" +
+                    "&Version=20131101";
+
+            System.out.println(url);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(url);
+
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getElementsByTagName("item");
+
+            System.out.println(nList.getLength() + "개의 데이터 발견");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    System.out.println("책 제목 : " + getTagvalue("title",eElement));
+                    System.out.println("작가 : " + getTagvalue("author",eElement));
+                    System.out.println("책 링크 : " + getTagvalue("link",eElement));
+                    System.out.println("책 정보 : " + getTagvalue("description",eElement));
+                    System.out.println();
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        GetAPIData gad = new GetAPIData();
+        gad.getApiData();
+
+    }
+
+}
